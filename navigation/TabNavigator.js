42dfd742e635c95dc45ffde21f6b3cbef6fc5e5b
AksharaTurlapati <79235627+AksharaTurlapati@users.noreply.github.com>
2022-09-24 20:51:11 -0400

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feed from "../screens/Feed";
+import CreatePost from "../screens/CreatePost";
+import createMaterialBottomTabNavigator from "@react-navigation/material-bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === 'Feed') {
+                        iconName = focused
+                            ? 'book'
+                            : 'book-outline';
+                    } else if (route.name === 'CreatePost') {
+                        iconName = focused ? 'create' : 'create-outline';
+                    }
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+            }}
+        >
+            <Tab.Screen name="Feed" component={Feed} />
+            <Tab.Screen name="CreatePost" component={CreatePost} />
+        </Tab.Navigator>
+    );
+}
+
+export default BottomTabNavigator
+
+
+const styels = styleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "black"
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? statusbar.currentHeight : RFValue(35)
+    },
+    appTitle: {
+        flex: 0.07,
+        flexDirection: "row"
+    },
+    appIcon: {
+        flex: 0.2,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    iconImage:{
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain"
+    },
+    appTitleTextContainer: {
+        flex: 0.8,
+        justifyContent: "center"
+    },
+    appTitleText: {
+        color: "white",
+        fontSize: PFValue(28)
+    },
+    cardContainer: {
+        flex: 0.85
+    }
+
+})
